d34edf58abb5de12e435ddf8129bb6c6fdd0bc2e
Christos Markos <cmarkos@noreply.platform.zone01.gr>
2024-05-29 22:02:31 +0000

    Upload files to ''

---
+++ explain.sh
@@ -0,0 +1,6 @@
+echo "FirstNameOfWitness LastNameOfWitness"
+echo "123456"
+echo "Red Ferrari"
+echo "FirstNameOfSuspect1 LastNameOfSuspect1"
+echo "FirstNameOfSuspect2 LastNameOfSuspect2"
+echo "FirstNameOfSuspect3 LastNameOfSuspect3"
