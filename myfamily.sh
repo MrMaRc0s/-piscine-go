e87cb9afec6a9593ad659d23feaa26b7933e58ed
Christos Markos <cmarkos@noreply.platform.zone01.gr>
2024-05-29 18:20:22 +0000

    Update 'myfamily.sh'

--- myfamily.sh
+++ myfamily.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-family=$(curl -s https://platform.zone01.gr/assets/superhero/all.json | jq '.[] | select(.id == $HERO_ID') | .connections.relatives')
-family=$(echo "$family" | sed 's/"//g')
-echo "$family"
+relatives=$(curl -s https://platform.zone01.gr/assets/superhero/all.json | jq '.[] | select(.id=='$HERO_ID') | .connections.relatives')
+relatives=$(echo "$relatives" | sed 's/"//g')
+echo "$relatives"
