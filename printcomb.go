9a95acbc47f38ba9bc5a2226375e29c8731eb74e
Christos Markos <cmarkos@noreply.platform.zone01.gr>
2024-05-30 21:16:45 +0000

    Update 'printcomb.go'

--- printcomb.go
+++ printcomb.go
@@ -4,21 +4,20 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func PrintComb() {
+func PrintComb2() {
 	first := true
-	for i := 0; i <= 7; i++ {
-		for j := i + 1; j <= 8; j++ {
-			for k := j + 1; k <= 9; k++ {
-				if !first {
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
-				}
-				first = false
-				z01.PrintRune(rune('0' + i))
-				z01.PrintRune(rune('0' + j))
-				z01.PrintRune(rune('0' + k))
+	for i := 0; i <= 98; i++ {
+		for j := i + 1; j <= 99; j++ {
+			if !first {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
 			}
+			first = false
+			z01.PrintRune(rune('0' + i/10))
+			z01.PrintRune(rune('0' + i%10))
+			z01.PrintRune(' ')
+			z01.PrintRune(rune('0' + j/10))
+			z01.PrintRune(rune('0' + j%10))
 		}
 	}
-	z01.PrintRune(rune('\n'))
-}
+	z01.PrintRune('\n')
