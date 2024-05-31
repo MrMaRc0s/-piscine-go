b6bbc573280849023a41e2de6e6e83721e7e7e98
Christos Markos <cmarkos@noreply.platform.zone01.gr>
2024-05-31 23:38:48 +0000

    Update 'printstr.go'

--- printstr.go
+++ printstr.go
@@ -3,7 +3,7 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(s string) {
-	for _, char := range s {
-		z01.PrintRune(char)
-	}
+    for i := 0; i < len(s); i++ {
+        z01.PrintRune(rune(s[i]))
+    }
 }
