54c097751519c271af7a84305e33105d6356bb5e
Christos Markos <cmarkos@noreply.platform.zone01.gr>
2024-06-01 00:25:31 +0000

    Update 'basicatoi.go'

--- basicatoi.go
+++ basicatoi.go
@@ -2,12 +2,9 @@ package piscine
 
 func BasicAtoi(s string) int {
     result := 0
-
     for i := 0; i < len(s); i++ {
-        // Convert the character to its numeric value by subtracting '0'
         digit := int(s[i] - '0')
         result = result*10 + digit
     }
-
     return result
 }
