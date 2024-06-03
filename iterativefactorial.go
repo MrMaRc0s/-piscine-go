04e1df3bcefd77bfdf727321e793000c146f4330
cmarkos <xristosssamarkosss@gmail.com>
2024-06-03 20:59:48 +0300

    zone01 :)

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,7 +2,7 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	for i := nb - 1; i > 0; i-- {
-		if nb < 0 || nb > 2147483647 {
+		if nb < 0 && nb > 2147483647 {
 			return 0
 		}
 		nb *= i
