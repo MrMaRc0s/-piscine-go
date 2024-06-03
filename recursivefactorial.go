6754cad7afb9c45dda27280512054e0a823ed1c3
cmarkos <xristosssamarkosss@gmail.com>
2024-06-03 22:02:08 +0300

    smart

---
+++ recursivefactorial.go
@@ -0,0 +1,12 @@
+package piscine
+
+func RecursiveFactorial(nb int) int {
+	NonPossibleValue := 20
+	if nb < 0 || nb > NonPossibleValue {
+		return 0
+	} else if nb == 0 {
+		return 1
+	}
+
+	return nb * (nb - 1)
+}
