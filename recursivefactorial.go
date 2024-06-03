dd9fcff32301b96cc10af30c2c9edfe7fb038111
cmarkos <xristosssamarkosss@gmail.com>
2024-06-03 22:03:38 +0300

    smart

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -4,7 +4,7 @@ func RecursiveFactorial(nb int) int {
 	NonPossibleValue := 20
 	if nb < 0 || nb > NonPossibleValue {
 		return 0
-	} else if nb == 0 {
+	} else if nb == 0 || nb == 1 {
 		return 1
 	}
 
