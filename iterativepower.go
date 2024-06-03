a83a364b9695a55ac813b26ec58dad5851d1bec8
Christos Markos <xristosssmarkosss@gmail.com>
2024-06-04 01:04:50 +0300

    True Power

--- iterativepower.go
+++ iterativepower.go
@@ -2,10 +2,12 @@ package piscine
 
 func IterativePower(nb int, power int) int {
 	result := 1
+	pw := power
+	num := nb
 
 	for power != 0 {
-		result *= nb
-		power -= 1
+		result *= num
+		pw -= 1
 	}
 	if result < 0 {
 		return 0
