e9371d71049753fd1771833ad50bd9bcaf48314b
Christos Markos <xristosssmarkosss@gmail.com>
2024-06-04 05:33:31 +0300

    zone01

--- sqrt.go
+++ sqrt.go
@@ -1,18 +1,14 @@
 package piscine
 
-import "math"
-
 func Sqrt(nb int) int {
-	if nb < 0 {
+	if nb < 2 {
+		return nb
+	}
+	var x float32 = float32(nb)
+	var y float32 = (x + (float32(nb) / x)) / 2.0
+	if (y - 1.0*float32(int(y/1.0))) != 0 {
 		return 0
 	}
-
-	sqrt := math.Sqrt(float64(nb))
-
-	if sqrt == float64(int(sqrt)) {
-		return int(sqrt)
-	}
-
-	return 0
+	return int(y)
 
 }
