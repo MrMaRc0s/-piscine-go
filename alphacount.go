fcab0c2f810ba030779d133b3d9f7200c224e527
Christos Markos <xristosssmarkosss@gmail.com>
2024-06-05 02:42:25 +0300

    zone01

--- alphacount.go
+++ alphacount.go
@@ -4,7 +4,7 @@ func AlphaCount(s string) int {
 	var counter []rune = []rune(s)
 	var temp int = 1
 	for i := 0; i < len(s); i++ {
-		if (counter[i] > 'A' && counter[i] < 'Z') || (counter[i] > 'a' && counter[i] < 'z') {
+		if (counter[i] > 'A' && counter[i] < 'Z') || (counter[i] > 'a' && counter[i] < 'z') && (counter[i] != ' ') {
 			temp++
 		}
 	}
