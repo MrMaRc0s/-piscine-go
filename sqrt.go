35e74439f8fe59f37c881dfd92d76defb88dc0bd
Christos Markos <xristosssmarkosss@gmail.com>
2024-06-04 05:12:06 +0300

    zone01

---
+++ sqrt.go
@@ -0,0 +1,18 @@
+package piscine
+
+import "math"
+
+func Sqrt(nb int) int {
+	if nb < 0 {
+		return 0
+	}
+
+	sqrt := math.Sqrt(float64(nb))
+
+	if sqrt == float64(int(sqrt)) {
+		return int(sqrt)
+	}
+
+	return 0
+
+}
