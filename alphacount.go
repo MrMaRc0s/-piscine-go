06c8d1bf72fb7433f03fd57df0884515027f9bed
Christos Markos <xristosssmarkosss@gmail.com>
2024-06-05 02:41:28 +0300

    zone01

--- alphacount.go
+++ alphacount.go
@@ -2,9 +2,9 @@ package piscine
 
 func AlphaCount(s string) int {
 	var counter []rune = []rune(s)
-	var temp int = 0
+	var temp int = 1
 	for i := 0; i < len(s); i++ {
-		if counter[i] > 'A' && counter[i] < 'z' {
+		if (counter[i] > 'A' && counter[i] < 'Z') || (counter[i] > 'a' && counter[i] < 'z') {
 			temp++
 		}
 	}
