302e40ee2bb0c50afa0df43f4c613a5d9b7cad60
Christos Markos <xristosssmarkosss@gmail.com>
2024-06-04 05:36:37 +0300

    zone01

--- sqrt.go
+++ sqrt.go
@@ -1,10 +1,10 @@
 package piscine
 
 func Sqrt(nb int) int {
-	if nb < 2 {
-		return nb
-	} else if nb < 0 {
+	if nb < 0 {
 		return 0
+	} else if nb < 2 {
+		return nb
 	}
 	var x float32 = float32(nb)
 	var y float32 = (x + (float32(nb) / x)) / 2.0
