dc12f84d45fc167b56850ef4294c049cb4b415c0
Christos Markos <xristosssmarkosss@gmail.com>
2024-06-04 06:22:08 +0300

    zone01

--- findnextprime.go
+++ findnextprime.go
@@ -4,12 +4,15 @@ func FindNextPrime(nb int) int {
 	if nb < 2 {
 		return 2
 	}
+	MaxPrimeNumber := 282589933
 	for i := 0; i < nb+i; i++ {
 		flag := true
 		result := nb + i
 		for j := 2; j < result-1; j++ {
 			if result%j == 0 {
 				flag = false
+			} else if result >= MaxPrimeNumber {
+				return 0
 			}
 		}
 		if flag {
