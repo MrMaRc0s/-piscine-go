7685f0ffd2580acf8e23bd26e4f360753ae9c8c5
Christos Markos <xristosssmarkosss@gmail.com>
2024-06-05 02:25:01 +0300

    zone01

--- compare.go
+++ compare.go
@@ -1,18 +1,11 @@
 package piscine
 
 func Compare(a, b string) int {
-	if a == b {
-		return 0
-	} else if a[0] != b[0] {
+	if a < b {
 		return -1
+	} else if a > b {
+		return 1
+	} else {
+		return 0
 	}
-	var temp int = 0
-	for i := 0; i < len(a)-1 && i < len(b)-1; i++ {
-		if a[i] == b[i] {
-			temp++
-		} else {
-			return temp
-		}
-	}
-	return temp
 }
