6919ca63bb51aebdf5c2ef6afa2ddf2ef9e104ba
cmarkos <xristosssmarkosss@gmail.com>
2024-06-05 21:05:43 +0300

    zone01

---
+++ isalpha.go
@@ -0,0 +1,11 @@
+package piscine
+
+func IsAlpha(s string) bool {
+	slice := []rune(s)
+	for i := 0; i < len(slice); i++ {
+		if slice[i] < '0' || slice[i] > 'z' {
+			return false
+		}
+	}
+	return true
+}
