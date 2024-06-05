013f5dd52406fd0450706f17c221d55b567c955a
Christos Markos <xristosssmarkosss@gmail.com>
2024-06-05 04:17:16 +0300

    zone01

--- index.go
+++ index.go
@@ -1,24 +1,21 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	var temp int = 0
-	for i := 0; i < len(s); i++ {
-		if s[i] == toFind[0] {
-			if len(toFind) > 1 {
-				temp++
-				for j := 1; j < len(toFind); j++ {
-					if s[i] == toFind[j] {
-						temp--
-						break
-					}
-				}
-			} else {
-				temp++
-			}
-		}
+	sLen := len(s)
+	toFindLen := len(toFind)
+
+	if toFindLen == 0 {
+		return 0
 	}
-	if temp == 0 {
+	if toFindLen > sLen {
 		return -1
 	}
-	return temp
+
+	for i := 0; i <= sLen-toFindLen; i++ {
+		if s[i:i+toFindLen] == toFind {
+			return i
+		}
+	}
+
+	return -1
 }
