2ea16d9cfe15cfbf50e35508e6fe60417299a1a3
cmarkos <xristosssmarkosss@gmail.com>
2024-06-05 21:31:39 +0300

    zone01

--- isprintable.go
+++ isprintable.go
@@ -3,7 +3,7 @@ package piscine
 func IsPrintable(s string) bool {
 	slice := []rune(s)
 	for i := 0; i < len(slice); i++ {
-		if slice[i] < '0' || slice[i] > 'z' {
+		if slice[i] < 32 || slice[i] > 126 {
 			return false
 		}
 	}
