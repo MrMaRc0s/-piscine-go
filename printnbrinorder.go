990a43622342b25cd719a74f727d3c0f41bda1ac
Christos Markos <xristosssmarkosss@gmail.com>
2024-06-06 05:25:42 +0300

    zone01

---
+++ printnbrinorder.go
@@ -0,0 +1,24 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+	if n < 0 {
+		return
+	}
+
+	var digitCount [10]int
+
+	for n > 0 {
+		digit := n % 10
+		digitCount[digit]++
+		n /= 10
+	}
+
+	for digit, count := range digitCount {
+		for i := 0; i < count; i++ {
+			z01.PrintRune(rune(digit + '0'))
+			z01.PrintRune(' ')
+		}
+	}
+}
