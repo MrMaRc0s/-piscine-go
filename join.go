7bf5654fe2b5f2ee358ce82f64f020d66501b2bf
Christos Markos <xristosssmarkosss@gmail.com>
2024-06-07 01:37:45 +0300

    zone01

--- join.go
+++ join.go
@@ -3,7 +3,11 @@ package piscine
 func Join(strs []string, sep string) string {
 	var str string
 	for i := 0; i < len(strs); i++ {
-		str += string(strs[i]) + sep + " "
+		if i == len(strs)-1 {
+			str += string(strs[i])
+		} else {
+			str += string(strs[i]) + sep + " "
+		}
 	}
 	return str
 }
