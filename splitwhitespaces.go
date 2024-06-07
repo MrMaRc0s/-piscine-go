02a2d22ded883585a37185655fab433a4d01b35b
cmarkos <xristosssmarkosss@gmail.com>
2024-06-07 16:18:01 +0300

    zone01

---
+++ splitwhitespaces.go
@@ -0,0 +1,16 @@
+package piscine
+
+func SplitWhiteSpaces(s string) []string {
+	var word string
+	var result []string
+	runes := []rune(s)
+	for i := 0; i < len(runes); i++ {
+		if runes[i] != ' ' && runes[i] != '\t' && runes[i] != '\n' {
+			word += string(runes[i])
+		} else {
+			result = append(result, word)
+			word = ""
+		}
+	}
+	return result
+}
