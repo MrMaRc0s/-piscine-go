ba1a997e0bf91e06ab76a6615563f2f299b14d4d
cmarkos <xristosssmarkosss@gmail.com>
2024-06-12 15:16:13 +0300

    zone01

--- issorted.go
+++ issorted.go
@@ -1,14 +1,16 @@
 package piscine
 
-func IsSorted(f func(a, b int) int, a []int) bool {
-	for i := 0; i < len(a)-1; i++ {
-		if f(a[i], a[i+1]) > 0 {
-			return false
-		}
-	}
-	return true
-}
-
 func f(a, b int) int {
 	return a - b
+}
+
+func IsSorted(arr []int, f func(a, b int) int) bool {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		diff := f(arr[i], arr[i+1])
+		if diff > 0 {
+			return false // Not sorted
+		}
+	}
+	return true // Sorted
 }
