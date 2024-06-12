425c824d4371b6ee209dc36d04527da7fd287ae9
cmarkos <xristosssmarkosss@gmail.com>
2024-06-12 15:18:34 +0300

    zone01

--- issorted.go
+++ issorted.go
@@ -1,16 +1,21 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	// Loop through the slice up to the second-last element (exclusive)
-	for i := 0; i < len(a)-1; i++ {
-		if f(a[i], a[i+1]) > 0 {
-			return false // Not sorted if f returns positive (a[i] > a[i+1])
+	n := len(a)
+	for i := 0; i < n-1; i++ {
+		cmp := f(a[i], a[i+1])
+		if cmp > 0 {
+			return false // Not sorted
 		}
 	}
-	return true // If no unsorted elements found, the slice is sorted
+	return true // Sorted
 }
 
-// Helper function for testing (ascending order)
 func f(a, b int) int {
-	return a - b
+	if a > b {
+		return 1
+	} else if a < b {
+		return -1
+	}
+	return 0
 }
