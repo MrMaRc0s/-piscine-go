819290b3f533ebe191644d8c49cfab448871dd58
cmarkos <xristosssmarkosss@gmail.com>
2024-06-12 15:07:18 +0300

    zone01

--- issorted.go
+++ issorted.go
@@ -1,18 +1,9 @@
 package piscine
 
 func IsSorted(a []int) bool {
-	f := func(a, b int) int {
-		if a > b {
-			return 1
-		} else if a == b {
-			return 0
-		}
-		return -1
-	}
-
 	n := len(a)
 	for i := 0; i < n-1; i++ {
-		if f(a[i], a[i+1]) > 0 {
+		if a[i] > a[i+1] {
 			return false
 		}
 	}
