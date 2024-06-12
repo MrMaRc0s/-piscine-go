3d140f318c5635132053e484551ce224bafd3f96
Christos Markos <cmarkos@noreply.platform.zone01.gr>
2024-06-12 11:45:02 +0000

    Upload files to 'displayfile'

---
+++ displayfile/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <filename>")
+		return
+	}
+
+	filename := os.Args[1]
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	fmt.Print(string(content))
+}
