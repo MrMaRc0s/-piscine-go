0b46730d5da1323c4fe69500a51317a388814ffe
Christos Markos <cmarkos@noreply.platform.zone01.gr>
2024-06-12 11:50:10 +0000

    Update 'cat/main.go'

---
+++ cat/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"io"
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func cat(r io.Reader) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := r.Read(buf)
+		if n > 0 {
+			for _, b := range buf[:n] {
+				z01.PrintRune(rune(b))
+			}
+		}
+		if err != nil {
+			if err != io.EOF {
+				printError("Error reading input")
+			}
+			break
+		}
+	}
+}
+
+func printError(msg string) {
+	for _, r := range msg {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func printString(msg string) {
+	for _, r := range msg {
+		z01.PrintRune(r)
+	}
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		cat(os.Stdin)
+		return
+	}
+
+	for _, filename := range os.Args[1:] {
+		file, err := os.Open(filename)
+		if err != nil {
+			printString("ERROR: open ")
+			printString(filename)
+			printString(": ")
+			printError(err.Error())
+			continue
+		}
+		cat(file)
+		file.Close()
+	}
+}
