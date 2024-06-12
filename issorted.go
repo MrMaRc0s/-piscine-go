df3f8f83602a2069c62e3dda2da959d890d38d9e
cmarkos <xristosssmarkosss@gmail.com>
2024-06-12 15:29:48 +0300

    zone01

--- issorted.go
+++ issorted.go
@@ -2,18 +2,29 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	n := len(a)
+	sortedAsc := true
+	sortedDesc := true
+
+	// Check ascending order
 	for i := 0; i < n-1; i++ {
 		cmp := f(a[i], a[i+1])
 		if cmp > 0 {
-			for j := n - 1; j <= 0; j-- {
-				cmp := f(a[j], a[j-1])
-				if cmp < 0 {
-					return false
-				}
-			}
+			sortedAsc = false
+			break
 		}
 	}
-	return true // Sorted
+
+	// Check descending order
+	for i := 0; i < n-1; i++ {
+		cmp := f(a[i], a[i+1])
+		if cmp < 0 {
+			sortedDesc = false
+			break
+		}
+	}
+
+	// If the slice is sorted in either ascending or descending order, return true
+	return sortedAsc || sortedDesc
 }
 
 func f(a, b int) int {
