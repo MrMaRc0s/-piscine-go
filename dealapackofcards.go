1e07f3cbb7be9ee5537aa5c77b2fc6d2d5e3a341
cmarkos <xristosssmarkosss@gmail.com>
2024-06-13 18:48:12 +0300

    hackathon

---
+++ dealapackofcards.go
@@ -0,0 +1,11 @@
+package piscine
+
+import "fmt"
+
+func DealAPackOfCards(deck []int) {
+	step := len(deck) / 4
+	fmt.Printf("Player 1: %v", deck[0:step])
+	fmt.Printf("Player 2: %v", deck[step+1:step*2])
+	fmt.Printf("Player 3: %v", deck[step*2:step*3])
+	fmt.Printf("Player 4: %v", deck[step*3:step*4])
+}
