3310124d30673df5a021135d4cc6f5e224672bda
cmarkos <xristosssmarkosss@gmail.com>
2024-06-13 11:45:47 +0300

    hackathon

---
+++ descendcomb.go
@@ -0,0 +1,27 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func DescendComb() {
+	first := true
+	for i := 99; i >= 0; i-- {
+		for j := i - 1; j >= 0; j-- {
+			if !first {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			}
+			first = false
+			printTwoDigit(i)
+			z01.PrintRune(' ')
+			printTwoDigit(j)
+		}
+	}
+	z01.PrintRune('\n')
+}
+
+func printTwoDigit(n int) {
+	z01.PrintRune(rune('0' + n/10))
+	z01.PrintRune(rune('0' + n%10))
+}
