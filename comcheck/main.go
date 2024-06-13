2e66bd566be7be68cb39e4f543f9921d03b9b5c3
cmarkos <xristosssmarkosss@gmail.com>
2024-06-14 02:10:24 +0300

    hackathon

--- comcheck/main.go
+++ comcheck/main.go
@@ -7,7 +7,6 @@ import (
 
 func main() {
 	file := os.Args[1:]
-	fmt.Println(file)
 	for i := 0; i < len(file); i++ {
 		if file[i] == "01" || file[i] == "galaxy" || file[i] == "galaxy 01" {
 			fmt.Println("Alert!!!")
