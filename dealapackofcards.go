df0b79a3f17276953827f7a822dac26737d74840
cmarkos <xristosssmarkosss@gmail.com>
2024-06-13 18:50:10 +0300

    hackathon

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -5,7 +5,7 @@ import "fmt"
 func DealAPackOfCards(deck []int) {
 	step := len(deck) / 4
 	fmt.Printf("Player 1: %v", deck[0:step])
-	fmt.Printf("Player 2: %v", deck[step+1:step*2])
+	fmt.Printf("Player 2: %v", deck[step:step*2])
 	fmt.Printf("Player 3: %v", deck[step*2:step*3])
 	fmt.Printf("Player 4: %v", deck[step*3:step*4])
 }
