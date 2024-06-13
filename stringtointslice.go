2658145bd09e17bf3eeccbdcfafb8ce2b7761b53
cmarkos <xristosssmarkosss@gmail.com>
2024-06-13 20:38:24 +0300

    hackathon

--- stringtointslice.go
+++ stringtointslice.go
@@ -2,8 +2,9 @@ package piscine
 
 func StringToIntSlice(str string) []int {
 	var temp []int
-	for i := 0; i < len(str); i++ {
-		temp = append(temp, int(str[i]))
+	var runes []rune = []rune(str)
+	for i := 0; i < len(runes); i++ {
+		temp = append(temp, int(runes[i]))
 	}
 	return temp
 }
