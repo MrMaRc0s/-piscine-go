e54d81cff0682fa99076c202d732d41e9f67322e
cmarkos <xristosssmarkosss@gmail.com>
2024-06-14 04:48:35 +0300

    hackathon

--- activebits.go
+++ activebits.go
@@ -1,7 +1,5 @@
 package piscine
 
-import "fmt"
-
 func ActiveBits(n int) int {
 	var binary []int
 	var count int
@@ -9,7 +7,6 @@ func ActiveBits(n int) int {
 	for i := n; i > 0; i /= 2 {
 		binary = append(binary, i%2)
 	}
-	fmt.Println(binary)
 	for i := 0; i < len(binary); i++ {
 		if binary[i] == 1 {
 			count++
