0b1195cb1b8f4436d7a3409b694ca75185155c8e
cmarkos <xristosssmarkosss@gmail.com>
2024-06-14 06:52:50 +0300

    hackathon

--- fixthemain/main.go
+++ fixthemain/main.go
@@ -32,12 +32,12 @@ func CloseDoor(ptrDoor *Door) bool {
 }
 
 func IsDoorOpen(door *Door) bool {
-	PrintStr("is the Door closed ?")
+	PrintStr("is the Door opened ?")
 	return door.state == OPEN
 }
 
 func IsDoorClose(ptrDoor *Door) bool {
-	PrintStr("is the Door opened ?")
+	PrintStr("is the Door closed ?")
 	return ptrDoor.state == CLOSE
 }
 
