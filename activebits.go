88a4306b425d42aa6112cdf23352698193905d74
cmarkos <xristosssmarkosss@gmail.com>
2024-06-14 04:45:07 +0300

    hackathon

--- activebits.go
+++ activebits.go
@@ -1,5 +1,16 @@
 package piscine
 
 func ActiveBits(n int) int {
-	return n / 2
+	var binary []int
+	var count int
+
+	for i := n; n > 0; n /= 2 {
+		binary = append(binary, i%2)
+	}
+	for i := 0; i < len(binary); i++ {
+		if binary[i] == 1 {
+			count++
+		}
+	}
+	return count
 }
