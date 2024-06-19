6ded76ee27666685371a842833ff7c5a2d7a9fcc
cmarkos <xristosssmarkosss@gmail.com>
2024-06-19 18:35:02 +0300

    zone01

---
+++ listremoveif.go
@@ -0,0 +1,19 @@
+package piscine
+
+func ListRemoveIf(l *List, data_ref interface{}) {
+	if l != nil {
+		current := l.Head
+		prev := l.Head
+		for current != nil && current.Data == data_ref {
+			l.Head = current.Next
+			current = l.Head
+		}
+		for current != nil {
+			if current.Data != data_ref {
+				prev = current
+			}
+			prev.Next = current.Next
+			current = prev.Next
+		}
+	}
+}
