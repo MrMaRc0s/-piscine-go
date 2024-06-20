a4e6f61cc691d67db3d8b5a84c17ec8f0a603d20
cmarkos <xristosssmarkosss@gmail.com>
2024-06-20 16:14:31 +0300

    Quest6

--- flags/main.go
+++ flags/main.go
@@ -1,3 +1,4 @@
+// Quest6
 package main
 
 import (
@@ -5,61 +6,73 @@ import (
 	"os"
 )
 
-func main() {
-	args := os.Args[1:] // Get command line arguments (excluding program name)
+// Helper function to check if a character is a valid flag
+func isFlag(char string) bool {
+	return len(char) >= 2 && (char[0] == '-' || char[0] == '-') && (char[1] == '-' || char[1] == 'i' || char[1] == 'o')
+}
 
-	// If no arguments or help flag, print options
-	if len(args) == 0 || args[0] == "--help" {
-		fmt.Println(`--insert
-  -i
-         This flag inserts the string into the string passed as argument.
---order
-  -o
-         This flag will behave like a boolean, if it is called it will order the argument.`)
+// Function to print help information
+func printHelp() {
+	fmt.Println("--insert")
+	fmt.Println("  -i")
+	fmt.Println("         This flag inserts the string into the string passed as argument.")
+	fmt.Println("--order")
+	fmt.Println("  -o")
+	fmt.Println("         This flag will behave like a boolean, if it is called it will order the argument.")
+}
+
+// Function to insert a string into another string
+func insertString(base, toInsert string) string {
+	return base + toInsert
+}
+
+// Function to order a string in ASCII order
+func orderString(input string) string {
+	// Convert string to slice of runes
+	runes := []rune(input)
+	n := len(runes)
+
+	// Simple sorting using bubble sort
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if runes[j] > runes[j+1] {
+				// Swap characters
+				runes[j], runes[j+1] = runes[j+1], runes[j]
+			}
+		}
+	}
+
+	return string(runes)
+}
+
+func main() {
+	args := os.Args[1:] // Get command-line arguments excluding the program name
+
+	// Show help if no arguments or if --help/-h is given
+	if len(args) == 0 || args[0] == "--help" || args[0] == "-h" {
+		printHelp()
 		return
 	}
 
-	// Initialize variables
-	var insertStr, inputStr string
-	var orderFlag bool
-
 	// Process flags and arguments
-	for i := 0; i < len(args); i++ {
-		switch args[i] {
-		case "--insert", "-i":
-			if i+1 < len(args) {
-				insertStr = args[i+1]
-				i++ // Skip next argument
+	var inputString string
+
+	for _, arg := range args {
+		if isFlag(arg) {
+			switch arg {
+			case "--insert", "-i":
+				if len(args) > 1 {
+					inputString = insertString(inputString, args[1])
+					args = args[1:] // Skip the next argument as it was inserted
+				}
+			case "--order", "-o":
+				inputString = orderString(inputString)
 			}
-		case "--order", "-o":
-			orderFlag = true
-		default:
-			inputStr = args[i]
+		} else {
+			inputString += arg
 		}
 	}
 
-	// Handle insertion
-	if insertStr != "" {
-		inputStr += insertStr
-	}
-
-	// Handle ordering
-	if orderFlag {
-		inputStr = sortString(inputStr)
-	}
-
-	fmt.Println(inputStr)
-}
-
-// Sort a string in ASCII order
-func sortString(s string) string {
-	runes := []rune(s)
-	for i := 0; i < len(runes); i++ {
-		for j := i + 1; j < len(runes); j++ {
-			if runes[i] > runes[j] {
-				runes[i], runes[j] = runes[j], runes[i]
-			}
-		}
-	}
-	return string(runes)
+	// Print the resulting string
+	fmt.Println(inputString)
 }
