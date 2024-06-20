1793853a2723a161cbd9b4126e8840a72415a66f
Christos Markos <xristosssmarkosss@gmail.com>
2024-06-20 14:36:33 +0300

    Quest2

--- printnbr.go
+++ printnbr.go
@@ -1,19 +1,37 @@
 // Quest2
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
+	// Handle zero case
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+
+	// Handle negative number
 	if n < 0 {
 		z01.PrintRune('-')
 		n = -n
 	}
 
-	// Convert the number to a string by recursively calling PrintNbr on the digits.
-	if n >= 10 {
-		PrintNbr(n / 10)
+	// To handle the minimum value of int
+	if n == -2147483648 {
+		z01.PrintRune('2')
+		n = 147483648
 	}
-	z01.PrintRune(rune(n%10 + '0'))
+
+	// Convert number to digits
+	digits := []rune{}
+	for n > 0 {
+		digit := n % 10
+		digits = append(digits, rune('0'+digit))
+		n /= 10
+	}
+
+	// Print digits in reverse order
+	for i := len(digits) - 1; i >= 0; i-- {
+		z01.PrintRune(digits[i])
+	}
 }
