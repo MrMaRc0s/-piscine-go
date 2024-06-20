25d97d43829d59b21d7299d703663a400463b1db
cmarkos <xristosssmarkosss@gmail.com>
2024-06-20 16:26:50 +0300

    Quest6

--- flags/main.go
+++ flags/main.go
@@ -15,10 +15,10 @@ func isFlag(s string) bool {
 func printHelp() {
 	fmt.Println("--insert")
 	fmt.Println("  -i")
-	fmt.Println("         This flag inserts the string into the string passed as argument.")
+	fmt.Println("	 This flag inserts the string into the string passed as argument.")
 	fmt.Println("--order")
 	fmt.Println("  -o")
-	fmt.Println("         This flag will behave like a boolean, if it is called it will order the argument.")
+	fmt.Println("	 This flag will behave like a boolean, if it is called it will order the argument.")
 }
 
 // Function to insert a string into another string
